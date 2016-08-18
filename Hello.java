8a5e67b9173ba004cb47586370bbcb912d37c099
bhugol <bhugol@qburst.com>
2016-08-18 13:43:35 +0530

    Try

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello Git. Let's see");
+		System.out.println("Changes after first commit");	
+		System.out.println("Changes after first commit");	
+
+}
+	}
+
